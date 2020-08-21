c0bf2ae8ce41edd5335d44dc9ddbd24cf15750cf
Carlos Castro <cpac20.4@gmail.com>
2020-08-21 22:51:01 +0100

    Adiciona comando quote

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # bot.py
 import os
 import random
+import json
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -9,6 +10,13 @@ load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
+
+quotes_path = os.getenv('QUOTES_PATH')
+
+with open(quotes_path, encoding='utf-8') as f:
+    quotes = json.load(f)
+
+
 
 @bot.event
 async def on_ready():
@@ -24,41 +32,34 @@ async def on_command_error(ctx, error):
         await ctx.send('You do not have the correct role for this command.')
 
 
-@bot.command(name='piada', help='Responds with a random quote')
-async def piada(ctx):
-    quotes = [
-        'Américo',
-        'Sergay',
-        'Abatuto'
-    ]
+@bot.command()
+@commands.has_any_role('♕ Sangue Real','Humanos')
+async def test(ctx, *args):
+    await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
 
-    response = random.choice(quotes)
+
+
+@bot.command(name='quote', help='Responds with a random quote')
+async def quote(ctx):
+
+    author, quote = random.choice(list(quotes.items()))
+
+    response = '"{}"\n- {}'.format(quote, author)
     await ctx.send(response)
 
 
-@bot.command(name='teste')
-@commands.has_role('♕ Sangue Real')
-async def teste(ctx):
-    await ctx.send('olá membro do sangue real',)
 
+@bot.command(name='addquote', help='Adds quote')
+async def addquote(ctx):
 
+    # TODO juntar quote
 
-@bot.command(name="abatido",
-                description="Quem será abatido?",
-                brief="Si sarà abattuto!",
-                aliases=["Abatido", "ABATIDO"],
-                pass_context=True)
-async def abatido(ctx):
-    possible_responses = [
-        'Abatuto',
-        'Cotes',
-        'Lotes',
-        'Lagastulia',
-        'LordEagle',
-        'Sergey',
-        'Ahhhhmerico'
-    ]
-    await ctx.send(random.choice(possible_responses) + ", " + ctx.message.author.mention)
+    with open(quotes_path, 'w', encoding='utf-8') as json_file:
+        json.dump(quotes, json_file)
+
+    response = '"{}"\n- {}'.format(quote, author)
+    await ctx.send(response)
+
 
 
 
